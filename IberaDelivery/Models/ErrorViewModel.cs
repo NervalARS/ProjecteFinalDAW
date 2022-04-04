df7ea2f39bcd51c96f4847dc45899aeb8a8f5282
DanielMartinLeite <daniel_martinleite@iescarlesvallbona.cat>
2022-04-04 19:58:20 +0200

    reset

--- IberaDelivery/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace IberaDelivery.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
