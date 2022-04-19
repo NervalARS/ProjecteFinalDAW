ec1cc8e13af90696b1d726abd183bf867cb19c36
NervalARS <56166644+NervalARS@users.noreply.github.com>
2022-04-19 17:08:26 +0200

    Actualiza vida de las sessiones

--- IberaDelivery/Program.cs
+++ IberaDelivery/Program.cs
@@ -19,7 +19,7 @@ builder.Services.AddDistributedMemoryCache();
 
 builder.Services.AddSession(options =>
 {
-    options.IdleTimeout = TimeSpan.FromSeconds(100);
+    options.IdleTimeout = TimeSpan.FromHours(24);
     options.Cookie.HttpOnly = true;
     options.Cookie.IsEssential = true;
 });
