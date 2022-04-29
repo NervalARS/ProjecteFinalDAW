d8fa952712fe94d6c1a251f1187f898ba4b56a97
Pol <pol_codinatayeda@iescarlesvallbona.cat>
2022-04-29 18:57:30 +0200

    taula_adress_creada

---
+++ IberaDelivery/Models/Adress.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace IberaDelivery.Models
+{
+    public partial class Adress
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; } = null!;
+        public string LastName { get; set; } = null!;
+        public string Country { get; set; } = null!;
+        public string City { get; set; } = null!;
+        public int PostalCode { get; set; }
+        public int TargetNumber { get; set; }
+        public DateTime ExpiryDate { get; set; }
+        public int Cvv { get; set; }
+        public int UserId { get; set; }
+
+        public virtual User User { get; set; } = null!;
+    }
+}
