0e48b8e8cc605d6ad216875619ec997912f18b9d
Pol <pol_codinatayeda@iescarlesvallbona.cat>
2022-05-03 17:15:40 +0200

    sql

--- iberiadb.sql
+++ iberiadb.sql
@@ -1,5 +1,13 @@
+-- DROP SCHEMA dbo;
 
-CREATE TABLE category (
+CREATE SCHEMA dbo;
+-- iberiadb.dbo.category definition
+
+-- Drop table
+
+-- DROP TABLE iberiadb.dbo.category;
+
+CREATE TABLE iberiadb.dbo.category (
 	id int IDENTITY(1,1) NOT NULL,
 	name varchar(255) COLLATE Modern_Spanish_CI_AS NOT NULL,
 	CONSTRAINT category_PK PRIMARY KEY (id)
@@ -12,7 +20,7 @@ CREATE TABLE category (
 
 -- DROP TABLE iberiadb.dbo.[user];
 
-CREATE TABLE [user] (
+CREATE TABLE iberiadb.dbo.[user] (
 	id int IDENTITY(1,1) NOT NULL,
 	first_name varchar(255) COLLATE Modern_Spanish_CI_AS NULL,
 	last_name varchar(255) COLLATE Modern_Spanish_CI_AS NULL,
@@ -25,29 +33,13 @@ CREATE TABLE [user] (
 );
 
 
--- iberiadb.dbo.[order] definition
-
--- Drop table
-
--- DROP TABLE iberiadb.dbo.[order];
-
-CREATE TABLE [order] (
-	id int IDENTITY(1,1) NOT NULL,
-	[date] datetime NOT NULL,
-	import decimal(38,0) NOT NULL,
-	user_id int NOT NULL,
-	CONSTRAINT order_PK PRIMARY KEY (id),
-	CONSTRAINT order_FK_1 FOREIGN KEY (user_id) REFERENCES [user](id) ON DELETE CASCADE ON UPDATE CASCADE
-);
-
-
 -- iberiadb.dbo.product definition
 
 -- Drop table
 
 -- DROP TABLE iberiadb.dbo.product;
 
-CREATE TABLE product (
+CREATE TABLE iberiadb.dbo.product (
 	id int IDENTITY(1,1) NOT NULL,
 	name varchar(255) COLLATE Modern_Spanish_CI_AS NOT NULL,
 	description varchar(255) COLLATE Modern_Spanish_CI_AS NULL,
@@ -57,8 +49,26 @@ CREATE TABLE product (
 	price decimal(38,0) NOT NULL,
 	iva decimal(38,0) NOT NULL,
 	CONSTRAINT product_PK PRIMARY KEY (id),
-	CONSTRAINT product_Category_FK FOREIGN KEY (category_id) REFERENCES category(id) ON DELETE CASCADE ON UPDATE CASCADE,
-	CONSTRAINT product_user_FK FOREIGN KEY (provider_id) REFERENCES [user](id) ON DELETE CASCADE ON UPDATE CASCADE
+	CONSTRAINT product_Category_FK FOREIGN KEY (category_id) REFERENCES iberiadb.dbo.category(id) ON DELETE CASCADE ON UPDATE CASCADE,
+	CONSTRAINT product_user_FK FOREIGN KEY (provider_id) REFERENCES iberiadb.dbo.[user](id) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+
+-- iberiadb.dbo.shipment definition
+
+-- Drop table
+
+-- DROP TABLE iberiadb.dbo.shipment;
+
+CREATE TABLE iberiadb.dbo.shipment (
+	id int IDENTITY(1,1) NOT NULL,
+	address varchar(255) COLLATE Modern_Spanish_CI_AS NOT NULL,
+	country varchar(255) COLLATE Modern_Spanish_CI_AS NOT NULL,
+	city varchar(255) COLLATE Modern_Spanish_CI_AS NOT NULL,
+	postal_code varchar(5) COLLATE Modern_Spanish_CI_AS NOT NULL,
+	user_id int NOT NULL,
+	CONSTRAINT adress_PK PRIMARY KEY (id),
+	CONSTRAINT adress_FK FOREIGN KEY (user_id) REFERENCES iberiadb.dbo.[user](id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 
@@ -68,14 +78,14 @@ CREATE TABLE product (
 
 -- DROP TABLE iberiadb.dbo.comments;
 
-CREATE TABLE comments (
+CREATE TABLE iberiadb.dbo.comments (
 	id int IDENTITY(1,1) NOT NULL,
 	contens text COLLATE Modern_Spanish_CI_AS NULL,
 	user_id int NOT NULL,
 	product_id int NOT NULL,
 	CONSTRAINT comments_PK PRIMARY KEY (id),
-	CONSTRAINT comments_ProductFK FOREIGN KEY (product_id) REFERENCES product(id) ON DELETE CASCADE ON UPDATE CASCADE,
-	CONSTRAINT comments_User_FK FOREIGN KEY (user_id) REFERENCES [user](id)
+	CONSTRAINT comments_ProductFK FOREIGN KEY (product_id) REFERENCES iberiadb.dbo.product(id) ON DELETE CASCADE ON UPDATE CASCADE,
+	CONSTRAINT comments_User_FK FOREIGN KEY (user_id) REFERENCES iberiadb.dbo.[user](id)
 );
 
 
@@ -85,12 +95,30 @@ CREATE TABLE comments (
 
 -- DROP TABLE iberiadb.dbo.[image];
 
-CREATE TABLE [image] (
+CREATE TABLE iberiadb.dbo.[image] (
 	id int IDENTITY(1,1) NOT NULL,
 	product_id int NOT NULL,
 	[image] image NOT NULL,
 	CONSTRAINT image_PK PRIMARY KEY (id),
-	CONSTRAINT image_Product_FK FOREIGN KEY (product_id) REFERENCES product(id) ON DELETE CASCADE ON UPDATE CASCADE
+	CONSTRAINT image_Product_FK FOREIGN KEY (product_id) REFERENCES iberiadb.dbo.product(id) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+
+-- iberiadb.dbo.[order] definition
+
+-- Drop table
+
+-- DROP TABLE iberiadb.dbo.[order];
+
+CREATE TABLE iberiadb.dbo.[order] (
+	id int IDENTITY(1,1) NOT NULL,
+	[date] datetime NOT NULL,
+	import decimal(38,0) NOT NULL,
+	user_id int NOT NULL,
+	shipment_id int NOT NULL,
+	CONSTRAINT order_PK PRIMARY KEY (id),
+	CONSTRAINT order_FK FOREIGN KEY (shipment_id) REFERENCES iberiadb.dbo.shipment(id),
+	CONSTRAINT order_FK_1 FOREIGN KEY (user_id) REFERENCES iberiadb.dbo.[user](id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 
@@ -100,13 +128,13 @@ CREATE TABLE [image] (
 
 -- DROP TABLE iberiadb.dbo.ln_order;
 
-CREATE TABLE ln_order (
+CREATE TABLE iberiadb.dbo.ln_order (
 	num_order int NOT NULL,
 	num_line int IDENTITY(1,1) NOT NULL,
 	quantity int NOT NULL,
 	total_import decimal(38,0) NOT NULL,
 	ref_product int NOT NULL,
 	CONSTRAINT ln_order_PK PRIMARY KEY (num_order,num_line),
-	CONSTRAINT ln_order_Order_FK FOREIGN KEY (num_order) REFERENCES [order](id) ON DELETE CASCADE ON UPDATE CASCADE,
-	CONSTRAINT ln_order_Product_FK FOREIGN KEY (ref_product) REFERENCES product(id)
+	CONSTRAINT ln_order_Order_FK FOREIGN KEY (num_order) REFERENCES iberiadb.dbo.[order](id) ON DELETE CASCADE ON UPDATE CASCADE,
+	CONSTRAINT ln_order_Product_FK FOREIGN KEY (ref_product) REFERENCES iberiadb.dbo.product(id)
 );
