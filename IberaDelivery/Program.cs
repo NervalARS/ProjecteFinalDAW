3a06858821a8a614ba8f21bdd6aa0cff5fc95652
Pol <pol_codinatayeda@iescarlesvallbona.cat>
2022-05-19 16:36:27 +0200

    ssdd

--- IberaDelivery/Program.cs
+++ IberaDelivery/Program.cs
@@ -39,8 +39,11 @@ else
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+if (!app.Environment.IsDevelopment())
+{
+    app.UseHttpsRedirection();
+}
 
-app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
